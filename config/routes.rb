8baa0f9f9cc58d2605659f9ab36ae9da3eeb70d8
Richard LaBrecque <richardlabreque@gmail.com>
2022-03-01 21:04:24 -0500

    delete unused routes and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,4 @@ Rails.application.routes.draw do
     resources :items, except:[:destroy], controller: 'merchant_items'
     resources :invoices, only:[:index, :show, :update], controller: 'merchant_invoices'
   end
-
-  resources :items, only:[:show, :update]
-  resources :invoices, only:[:show]
 end
