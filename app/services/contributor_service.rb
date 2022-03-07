14feeefb4bb46993f83f21ce6258b72b77adf8f1
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-07 13:35:06 -0500

    remove api from previous project

--- app/services/contributor_service.rb
+++ app/services/contributor_service.rb
@@ -1,11 +1,11 @@
-class ContributorService
-  def self.conn
-    url = "https://api.github.com"
-    Faraday.new(url)
-  end
-
-  def self.search_contributors
-    response = conn.get("/repos/sullybirashk/little-esty-shop/contributors")
-    JSON.parse(response.body, symbolize_names: true)
-  end
-end
+# class ContributorService
+#   def self.conn
+#     url = "https://api.github.com"
+#     Faraday.new(url)
+#   end
+#
+#   def self.search_contributors
+#     response = conn.get("/repos/sullybirashk/little-esty-shop/contributors")
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
+# end
