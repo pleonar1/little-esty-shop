3524639a12b3304d1239c3a6eb077b2c8845a921
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-08 17:49:10 -0500

    Fix test and pass best_bulk_discount method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,4 +8,9 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :quantity
   validates_presence_of :unit_price
   validates_presence_of :status
+
+  def best_bulk_discount
+    bulk_discounts.where('quantity <= ?', quantity)
+    .order('bulk_discounts.discount').last
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -23,15 +23,16 @@ RSpec.describe InvoiceItem, type: :model do
       customer_1 = Customer.create!(first_name: "Person 1", last_name: "Mcperson 1")
 
       invoice_1 = customer_1.invoices.create!(status: "completed")
+      invoice_2 = customer_1.invoices.create!(status: "completed")
 
       invoice_item_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 100, unit_price: 100, status: "shipped")
-      invoice_item_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 1, unit_price: 100, status: "shipped")
+      invoice_item_2 = InvoiceItem.create!(invoice_id: invoice_2.id, item_id: item_1.id, quantity: 1, unit_price: 100, status: "shipped")
 
       bulk_discount1 = merchant_1.bulk_discounts.create!(discount: 20, quantity: 10)
       bulk_discount2 = merchant_1.bulk_discounts.create!(discount: 30, quantity: 100)
 
-      expect(invoice_item1.best_bulk_discount).to eq(bulk_discount2)
-      expect(invoice_item2.best_bulk_discount).to eq(nil)
+      expect(invoice_item_1.best_bulk_discount).to eq(bulk_discount2)
+      expect(invoice_item_2.best_bulk_discount).to eq(nil)
     end
   end
 end
